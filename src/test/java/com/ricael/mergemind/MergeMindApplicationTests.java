f837d0dca19504f7f725c6e7296efde63922abd6
Ricael <ricaelmenezes@gmail.com>
2025-12-25 16:12:36 -0300

    feat: add project on repository

---
+++ src/test/java/com/ricael/mergemind/MergeMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricael.mergemind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergeMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
